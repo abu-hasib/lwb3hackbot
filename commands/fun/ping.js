6365ac11c064e9f0fc98c0104a15324f10f44e03
Ridwan Abiola <ricklick262@gmail.com>
2023-10-04 14:20:57 +0100

    add redis

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,4 +1,13 @@
 const { SlashCommandBuilder } = require("discord.js");
+const cloudinary = require("cloudinary");
+const { redClient } = require("../../redis");
+
+cloudinary.v2.config({
+  cloud_name: "testuser",
+  api_key: "843265632781972",
+  api_secret: "iQGhBkE2GyDvlQTNDeQrLG111BQ",
+  secure: true,
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,12 +17,31 @@ module.exports = {
       option.setName("file").setDescription("File to upload")
     ),
   async execute(interaction) {
-    const file = interaction.options.getAttachment('file');
-		console.log({file})
+    const file = interaction.options.getAttachment("file");
+    console.log({ file });
 
-		if(!file) return
+    if (!file) return;
+    console.log({ interaction: interaction.user });
+    const user = interaction.user;
+    const { id, username } = user;
 
-		await interaction.reply(`Your file ${file.attachment}`);
-		// await interaction.guild.members.ban(target);
+    try {
+      const result = await cloudinary.v2.uploader.upload(`${file.attachment}`, {
+        public_id: "discord-server",
+      });
+      console.log({ result });
+      const res = await redClient.json.set(`file:${result.asset_id}`, '$', {
+        "username": username,
+        "id": id,
+        "url": `${result.url}`,
+        "bot": `bolda`
+      })
+      console.log({res})
+    } catch (error) {
+      console.error(error);
+    }
+
+    await interaction.reply(`Your file ${file.attachment}`);
+    // await interaction.guild.members.ban(target);
   },
 };
--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 require("dotenv").config();
 const fs = require("node:fs");
 const path = require("node:path");
-const fcl = require("@onflow/fcl")
+const fcl = require("@onflow/fcl");
 const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 const token = process.env.DISCORD_TOKEN;
+const { createClient, SchemaFieldTypes } = require("redis");
+const { redClient } = require("./redis");
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 fcl.config({
-  'accessNode.api': 'https://rest-testnet.onflow.org',
-  'discovery.wallet': `https://fcl-discovery.onflow.org/f8d6e0586b0a20c7/authn`,
-})
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "discovery.wallet": `https://fcl-discovery.onflow.org/f8d6e0586b0a20c7/authn`,
+});
 
-
-
-
-
-client.cooldowns = new Collection()
+client.cooldowns = new Collection();
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
@@ -57,20 +55,91 @@ for (const file of eventFiles) {
 
 client.login(token);
 
-
-
-module.exports = {client }
+module.exports = { client };
 
 async function runFcl() {
-  const res  = await fcl.query({
+  await redClient.connect();
+  // await redClient.set("key", "value");
+  // const value = await redClient.get("key");
+  // console.log({ value });
+
+  // try {
+  //   await redClient.ft.create(
+  //     "idx:files",
+  //     {
+  //       "$.username": {
+  //         type: SchemaFieldTypes.TEXT,
+  //         SORTABLE: true,
+  //       },
+  //       "$.id": {
+  //         type: SchemaFieldTypes.NUMERIC,
+  //         AS: "id",
+  //       },
+  //       "$.url": {
+  //         type: SchemaFieldTypes.TEXT,
+  //       },
+  //     },
+  //     {
+  //       ON: "JSON",
+  //       PREFIX: "file:",
+  //     }
+  //   );
+  // } catch (e) {
+  //   if (e.message === "Index already exists") {
+  //     console.log("Index exists already, skipped creation.");
+  //   } else {
+  //     // Something went wrong, perhaps RediSearch isn't installed...
+  //     console.error("error here");
+  //     console.error(e);
+  //     process.exit(1);
+  //   }
+  // }
+
+//   await Promise.all([
+//     redClient.json.set('file:1', '$', {
+//         "username": "abuhasib",
+//         "id": 90,
+//         "url": "http://localhost:3000",
+//         "bot": "bolda"
+//     }),
+//     redClient.json.set('file:2', '$', {
+//         "username": "edenz",
+//         "id": 90,
+//         "url": "http://localhost:3000",
+//         "bot": "bolda"
+//     }),
+//     redClient.json.set('file:3', '$', {
+//       "username": "pozoo",
+//       "id": 24,
+//       "url": "http://localhost:3000",
+//       "bot": "bolda"
+//     }),
+// ]);
+
+let result = await redClient.ft.search(
+  'idx:files',
+  'abuhasib'
+);
+console.log(JSON.stringify(result, null, 2));
+
+// result = await redClient.ft.search(
+//   'idx:users',
+//   'Paul @age:[30 40]',
+//   {
+//       RETURN: ['$.city']
+//   }
+// );
+// console.log(JSON.stringify(result, null, 2));
+
+  const res = await fcl.query({
     cadence: `
       import CryptoPoops from 0x2cd15cc7890127f9
   
       pub fun main(): UInt64 {
         return CryptoPoops.totalSupply
       }
-  `
+  `,
   });
 }
 
-runFcl()
+runFcl();
